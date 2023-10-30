20c88fc647fa86f8f7b0ad3bdb241523c8f830ac
Nezox56 <36198892+Nezox56@users.noreply.github.com>
2023-10-30 11:52:04 +0500

    свел в один запрос

--- DataBaseRequest.cs
+++ DataBaseRequest.cs
@@ -11,10 +11,7 @@ namespace Grout
 {
     internal class DataBaseRequest
     {
-        //public const string connectionMudDBTest = "Server=(localdb)\\mssqllocaldb;Database=MudDBTest;Trusted_Connection=True;";
-        //public const string connectionMaster = "Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=True;";
-        
-        public const string _masterString = "master";
+         public const string _masterString = "master";
         public const string _mudDBTestString = "MudDBTest";
 
         //проверка на существование БД
@@ -29,54 +26,51 @@ namespace Grout
                 }
                 catch
                 {
-                    CreateDB(_masterString, _mudDBTestString);                    
-                    CreateTable(_mudDBTestString, "CREATE TABLE Grout ([Id] INT PRIMARY KEY IDENTITY, [Name] NVARCHAR(255) NOT NULL, [Volume] INT NOT NULL)");
-                    CreateTable(_mudDBTestString, "CREATE TABLE Structure ([Id] INT PRIMARY KEY IDENTITY, [Name] NVARCHAR(255) NOT NULL, [Quantity] DECIMAL(3,2) NOT NULL, [Id_grout] INT NOT NULL References Grout(Id))");
+                    await CreateDB();
                 }
             }
         }
 
-
-        // Создание БД
-        public static async void CreateDB(string connectionStringDB, string nameDB)
+        // Создание БД и таблиц
+        public static async Task CreateDB()
         {
             try
             {
-                string connectionString = $"Server=(localdb)\\mssqllocaldb; Database={connectionStringDB}; Trusted_Connection=true;";
+                string connectionString = "Server=(localdb)\\mssqllocaldb; Database=master; Trusted_Connection=true;";
+
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     await connection.OpenAsync();
 
-                    string createDbQuery = $"CREATE DATABASE {nameDB}";
+                    string createDbQuery = "CREATE DATABASE MudDBTest";
                     SqlCommand command = new SqlCommand();
                     command.CommandText = createDbQuery;
                     command.Connection = connection;
                     await command.ExecuteNonQueryAsync();
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
 
-        //Создание таблицы
-        public static async void CreateTable(string connectionStringDB, string queryTable)
-        {
-            try
-            {
-                string connectionString = $"Server=(localdb)\\mssqllocaldb; Database={connectionStringDB}; Trusted_Connection=true;";
+                connectionString = "Server=(localdb)\\mssqllocaldb; Database=MudDBTest; Trusted_Connection=true;";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     await connection.OpenAsync();
 
-                    string createTableQuery = queryTable; 
+                    string createTableQuery = "CREATE TABLE Grout ([Id] INT PRIMARY KEY IDENTITY, [Name] NVARCHAR(255) NOT NULL, [Volume] INT NOT NULL)";
                     SqlCommand command = new SqlCommand();
                     command.CommandText = createTableQuery;
                     command.Connection = connection;
                     await command.ExecuteNonQueryAsync();
                 }
 
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    await connection.OpenAsync();
+
+                    string createTableQuery = "CREATE TABLE Structure ([Id] INT PRIMARY KEY IDENTITY, [Name] NVARCHAR(255) NOT NULL, [Quantity] DECIMAL(3,2) NOT NULL, [Id_grout] INT NOT NULL References Grout(Id))";
+                    SqlCommand command = new SqlCommand();
+                    command.CommandText = createTableQuery;
+                    command.Connection = connection;
+                    await command.ExecuteNonQueryAsync();
+                }
             }
             catch (Exception ex)
             {
