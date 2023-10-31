0ddd625af9c24908b73d1e84b3276b0eb64c2a35
Nezox56 <36198892+Nezox56@users.noreply.github.com>
2023-10-31 18:58:23 +0500

    методы возвращают Task, вызываются через await | проблема с снхронизацией осталась

--- DataBaseRequest.cs
+++ DataBaseRequest.cs
@@ -11,11 +11,14 @@ namespace Grout
 {
     internal class DataBaseRequest
     {
-         public const string _masterString = "master";
+        //public const string connectionMudDBTest = "Server=(localdb)\\mssqllocaldb;Database=MudDBTest;Trusted_Connection=True;";
+        //public const string connectionMaster = "Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=True;";
+
+        public const string _masterString = "master";
         public const string _mudDBTestString = "MudDBTest";
 
         //проверка на существование БД
-        public static async void CheckConnection(string connectionStringDB)
+        public static async Task CheckConnection(string connectionStringDB)
         {
             string connectionString = $"Server=(localdb)\\mssqllocaldb; Database={connectionStringDB}; Trusted_Connection=true;";
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -26,51 +29,57 @@ namespace Grout
                 }
                 catch
                 {
-                    await CreateDB();
+                    await CreateDB(_masterString, _mudDBTestString);
                 }
             }
         }
 
-        // Создание БД и таблиц
-        public static async Task CreateDB()
+
+        // Создание БД
+        public static async Task CreateDB(string connectionStringDB, string nameDB)
         {
             try
             {
-                string connectionString = "Server=(localdb)\\mssqllocaldb; Database=master; Trusted_Connection=true;";
-
+                string connectionString = $"Server=(localdb)\\mssqllocaldb; Database={connectionStringDB}; Trusted_Connection=true;";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     await connection.OpenAsync();
 
-                    string createDbQuery = "CREATE DATABASE MudDBTest";
+                    string createDbQuery = $"CREATE DATABASE {nameDB}";
                     SqlCommand command = new SqlCommand();
                     command.CommandText = createDbQuery;
                     command.Connection = connection;
                     await command.ExecuteNonQueryAsync();
                 }
+                    await CreateTable(_mudDBTestString, "CREATE TABLE Grout ([Id] INT PRIMARY KEY IDENTITY, " +
+                        "[Name] NVARCHAR(255) NOT NULL, [Volume] INT NOT NULL)");
+                    await CreateTable(_mudDBTestString, "CREATE TABLE Structure ([Id] INT PRIMARY KEY IDENTITY, " +
+                        "[Name] NVARCHAR(255) NOT NULL, [Quantity] DECIMAL(3,2) NOT NULL, " +
+                        "[Id_grout] INT NOT NULL References Grout(Id))");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
-                connectionString = "Server=(localdb)\\mssqllocaldb; Database=MudDBTest; Trusted_Connection=true;";
+        //Создание таблицы
+        public static async Task CreateTable(string connectionStringDB, string queryTable)
+        {
+            try
+            {
+                string connectionString = $"Server=(localdb)\\mssqllocaldb; Database={connectionStringDB}; Trusted_Connection=true;";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     await connection.OpenAsync();
 
-                    string createTableQuery = "CREATE TABLE Grout ([Id] INT PRIMARY KEY IDENTITY, [Name] NVARCHAR(255) NOT NULL, [Volume] INT NOT NULL)";
+                    string createTableQuery = queryTable;
                     SqlCommand command = new SqlCommand();
                     command.CommandText = createTableQuery;
                     command.Connection = connection;
                     await command.ExecuteNonQueryAsync();
                 }
 
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    await connection.OpenAsync();
-
-                    string createTableQuery = "CREATE TABLE Structure ([Id] INT PRIMARY KEY IDENTITY, [Name] NVARCHAR(255) NOT NULL, [Quantity] DECIMAL(3,2) NOT NULL, [Id_grout] INT NOT NULL References Grout(Id))";
-                    SqlCommand command = new SqlCommand();
-                    command.CommandText = createTableQuery;
-                    command.Connection = connection;
-                    await command.ExecuteNonQueryAsync();
-                }
             }
             catch (Exception ex)
             {
