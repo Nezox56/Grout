183a17a4745e165801af18cd23a0ca6de2240820
Nezox56 <36198892+Nezox56@users.noreply.github.com>
2023-11-07 01:40:58 +0500

    .

--- DataBaseRequest.cs
+++ DataBaseRequest.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static Grout.MainForm;
